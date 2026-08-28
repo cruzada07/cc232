51c67ff2f7d9a791e0276718b4bdd336d8df70ed
Kevin <kevindelacruzsanchez596@gmail.com>
2026-08-27 19:40:55 -0500

    feat: terminar algoritmo

--- semana1/busqueda_secuencial.cpp
+++ semana1/busqueda_secuencial.cpp
@@ -2,6 +2,23 @@
 using namespace std;
 
 int main(){
-    cout<<"Hola";
+    int arr[] = {12,16,4,27,8,73};
+    bool band = false;
+    int i=0, num;
+    cout<<"Ingrese un numero: "; cin>>num;
+
+    while((band==false) && (i<6)){
+        if(arr[i] == num){
+            band = true;
+        }
+        i++;
+    }
+
+    if(band){
+        cout<<"Su numero fue encontrado en la posicion "<<i-1<<endl;
+    }else{
+        cout<<"Su numero no fue entcontrado"<<endl;
+    }
+
     return 0;
 }
